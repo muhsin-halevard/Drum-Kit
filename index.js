a860c50a07c5d0c4e73f001806e6e396c3892276
muhsin-halevard <133557676+muhsin-halevard@users.noreply.github.com>
2023-12-11 18:52:20 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+
+var buttons = document.querySelectorAll("button");
+
+for (var i = 0; i < buttons.length; i++) {
+buttons[i].addEventListener("click", handleClick);
+}
+function handleClick(){
+var buttonInnerHTML = this.innerHTML;
+
+makeSound(buttonInnerHTML);
+
+}
+
+document.addEventListener("keydown", function(event){
+  makeSound(event.key);
+});
+
+function makeSound (key){
+  switch (key) {
+    case "w":
+    var top1 = new Audio("sounds/tom-1.mp3");
+    top1.play();
+      break;
+
+    case "a":
+    var top2 = new Audio("sounds/tom-2.mp3");
+    top2.play();
+      break;
+
+    case "s":
+    var top3 = new Audio("sounds/tom-3.mp3");
+    top3.play();
+      break;
+
+    case "d":
+    var top4 = new Audio("sounds/tom-4.mp3");
+    top4.play();
+      break;
+
+    case "j":
+    var crash = new Audio("sounds/crash.mp3");
+    crash.play();
+      break;
+
+    case "k":
+    var bass = new Audio("sounds/kick-bass.mp3");
+    bass.play();
+      break;
+
+    case "l":
+    var snare = new Audio("sounds/snare.mp3");
+    snare.play();
+      break;
+
+        default: console.log(buttonInnerHTML);
+
+  }
+}
